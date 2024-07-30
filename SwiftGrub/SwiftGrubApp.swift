770c15b8ffed87fd0983d4263cce2579eb039900
Chi-Hsien Wu <chwu891@gmail.com>
2024-07-29 22:03:34 -0700

    Initial Commit

---
+++ SwiftGrub/SwiftGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftGrubApp.swift
+//  SwiftGrub
+//
+//  Created by Chi-Hsien Wu on 7/29/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
