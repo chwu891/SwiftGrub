c833bc289ff3fa5f088c6a05a23ed38a4ee35e3c
Chi-Hsien Wu <chwu891@gmail.com>
2024-07-31 21:09:24 -0700

    Create custom AlertItem

---
+++ SwiftGrub/Utilities/AlertItem.swift
@@ -0,0 +1,24 @@
+//
+//  AlertItem.swift
+//  SwiftGrub
+//
+//  Created by Chi-Hsien Wu on 7/31/24.
+//
+
+import SwiftUI
+
+struct AlertItem: Identifiable {
+    let id = UUID()
+    let title: Text
+    let message: Text
+    let dismissButton: Alert.Button
+}
+
+struct AlertContext {
+    
+    //MARK: - MapView Errors
+    static let unableToGetLocations = AlertItem(title: Text("Locations Error"),
+                                                message: Text("Unable to retrieve locations at this time.\nPlease try again."),
+                                                dismissButton: .default(Text("Ok")))
+}
+
