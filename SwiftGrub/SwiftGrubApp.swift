a9f805f174edaeb4967cdb517986d96d39a2e3ca
Chi-Hsien Wu <chwu891@gmail.com>
2024-08-01 10:36:56 -0700

    Create EnvironmentObject for locations and add Map Pins

--- SwiftGrub/SwiftGrubApp.swift
+++ SwiftGrub/SwiftGrubApp.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 @main
 struct SwiftGrubApp: App {
+    
+    let locationManager = LocationManager()
+    
     var body: some Scene {
         WindowGroup {
             AppTabView()
+                .environmentObject(locationManager)
         }
     }
 }
---
+++ SwiftGrub/Utilities/Managers/LocationManager.swift
@@ -0,0 +1,12 @@
+//
+//  LocationManager.swift
+//  SwiftGrub
+//
+//  Created by Chi-Hsien Wu on 8/1/24.
+//
+
+import Foundation
+
+final class LocationManager: ObservableObject {
+    @Published var locations: [DDGLocation] = []
+}
