04ed2671399b015bd018565071374ef9e2e9aff0
Chi-Hsien Wu <chwu891@gmail.com>
2024-08-02 14:49:00 -0700

    Create ProfileModalView

---
+++ SwiftGrub/Views/Screens/LocationDetailView/ProfileModalView.swift
@@ -0,0 +1,52 @@
+//
+//  ProfileModalView.swift
+//  SwiftGrub
+//
+//  Created by Chi-Hsien Wu on 8/2/24.
+//
+
+import SwiftUI
+
+struct ProfileModalView: View {
+    var body: some View {
+        ZStack {
+            VStack {
+                Spacer().frame(height: 60)
+                Text("Sean Allen")
+                    .bold()
+                    .font(.title2)
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.75)
+                
+                Text("Test Company")
+                    .fontWeight(.semibold)
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.75)
+                    .foregroundColor(.secondary)
+                
+                Text("This is my sample bio. Let's keep typing to see how long we can make this, how does the padding look.")
+                    .lineLimit(3)
+                    .padding()
+            }
+            .frame(width: 300, height: 230)
+            .background(Color(.secondarySystemBackground))
+            .cornerRadius(16)
+            .overlay(Button {
+                // dismiss
+            } label: {
+                XDismissButton()
+            }, alignment: .topTrailing)
+            
+            Image(uiImage: PlaceholderImage.avatar)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 110, height: 110)
+                .clipShape(Circle())
+                .shadow(color: .black.opacity(0.5), radius: 4, x: 0, y: 6)
+                .offset(y: -120)
+        }
+    }
+}
+#Preview {
+    ProfileModalView()
+}
